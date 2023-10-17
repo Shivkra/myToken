27b5bdb3b39785fb75318c81b6f3cfb704fcb8ef
Shivkra <109886104+Shivkra@users.noreply.github.com>
2023-10-17 22:39:57 +0530

    Update newToken.sol

--- newToken.sol
+++ newToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract newToken is ERC20 {
-constructor () ERC20("New World Coin", "NWC") {
+constructor () ERC20("New World Coin", "New") {
 _mint(msg.sender,
     1000000 * 10 ** decimals());
   }
